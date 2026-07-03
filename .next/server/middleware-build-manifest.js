b232d3677fad0b3a922e57ec2b4650f4d5a699db
DJBAHATI <peterbahati0455033@gmail.com>
2026-07-03 13:50:06 +0200

    delete the following lines from the commit message:

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,18 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-8b785e33a384d227.js","static/chunks/fd9d1056-8bac9e7f66a33d92.js","static/chunks/117-f8b57b9810db7bae.js","static/chunks/main-app-bf60b8de12dc7160.js"],pages:{"/_app":["static/chunks/webpack-8b785e33a384d227.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-012ff81dd0d23378.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-8b785e33a384d227.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-012ff81dd0d23378.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
